53fa9eb3cef323c9b86e2cece812a01468d787f1
leo <467512138@qq.com>
2026-08-04 23:03:11 +0800

    upadet

--- server/src/services/wechat.js
+++ server/src/services/wechat.js
@@ -1,4 +1,33 @@
+const https = require('https')
 const { config } = require('../config')
+
+/**
+ * 简易 GET JSON（兼容 Node 16，不依赖全局 fetch）
+ * @param {string | URL} url
+ * @returns {Promise<object>}
+ */
+function getJson(url) {
+  return new Promise((resolve, reject) => {
+    const req = https.get(String(url), (res) => {
+      let raw = ''
+      res.setEncoding('utf8')
+      res.on('data', (chunk) => {
+        raw += chunk
+      })
+      res.on('end', () => {
+        try {
+          resolve(JSON.parse(raw))
+        } catch (err) {
+          reject(Object.assign(new Error('微信接口返回非 JSON'), { status: 502, detail: raw }))
+        }
+      })
+    })
+    req.on('error', reject)
+    req.setTimeout(15000, () => {
+      req.destroy(new Error('请求微信接口超时'))
+    })
+  })
+}
 
 /**
  * 用 wx.login 拿到的 code 换取 openid / session_key
@@ -24,8 +53,7 @@ async function code2Session(code) {
   url.searchParams.set('js_code', code)
   url.searchParams.set('grant_type', 'authorization_code')
 
-  const response = await fetch(url)
-  const data = await response.json()
+  const data = await getJson(url)
 
   if (data.errcode) {
     throw Object.assign(new Error(data.errmsg || `微信登录失败(${data.errcode})`), {
