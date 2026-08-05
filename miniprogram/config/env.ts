fe7adee767c6f6a2c05e36fd1cf958b9537841e3
leo <467512138@qq.com>
2026-08-05 21:36:44 +0800

    提交代码

--- miniprogram/config/env.ts
+++ miniprogram/config/env.ts
@@ -1,5 +1,5 @@
 /** 云端 API 基础地址。真机调试请改成可访问的局域网 IP 或已备案域名。 */
-export const API_BASE_URL = 'http://127.0.0.1:4000'
+export const API_BASE_URL = 'https://www.ushow.ink/4000'
 
 export const AUTH_STORAGE_KEYS = {
   token: 'hanzi_cloud_token',
